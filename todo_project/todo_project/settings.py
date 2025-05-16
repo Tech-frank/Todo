005fa73ea831ba93ff8817018499e7c95d0fba4f
franklyn Etu <franklynetu80@gmail.com>
2025-05-16 11:40:56 +0100

    new allowed hosts

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['https://todo-bk61.onrender.com']  # For now, to be updated after deploy
+ALLOWED_HOSTS = ['todo-bk61.onrender.com']  # For now, to be updated after deploy
 
 
 
